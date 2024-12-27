31799725b0e9a3ee8e9a723dc70e29291d854d95
Mobilevpn <68244824+FBI0101@users.noreply.github.com>
2024-12-26 23:56:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
 import yt_dlp as youtube_dl
+import tempfile
+import shutil
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
-# Substitua com o token do seu bot
-TOKEN = '7329791456:AAFd7GHgWxNey2FWdGpas5J-bvJvs3fuwFc'
+TOKEN = 'seu-token-aqui'
 
-# Função para baixar o vídeo usando yt-dlp
 def download_video(url, download_path):
     ydl_opts = {
-        'format': 'best',  # Melhor formato disponível sem mesclagem
-        'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s'),  # Nome do arquivo
+        'format': 'best',
+        'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s'),
         'quiet': False,
     }
 
@@ -23,38 +23,30 @@ def download_video(url, download_path):
             print(f"Erro ao baixar o vídeo: {e}")
             return None
 
-# Comando /start
 async def start(update: Update, context):
     await update.message.reply_text('Olá! Envie um link do YouTube para baixar o vídeo.')
 
-# Handler para links de vídeos
 async def handle_video(update: Update, context):
     url = update.message.text
     if 'youtube.com' in url or 'youtu.be' in url:
         await update.message.reply_text('Baixando o vídeo, por favor, aguarde...')
-        download_path = os.path.join(os.getcwd(), 'downloads')  # Diretório para salvar os vídeos
+        download_path = tempfile.mkdtemp()  # Diretório temporário
 
-        # Certificar-se de que o diretório existe
-        if not os.path.exists(download_path):
-            os.makedirs(download_path)
-
-        # Baixar o vídeo
-        video_file = download_video(url, download_path)
-        if video_file:
-            # Enviar o vídeo de volta para o Telegram
-            await update.message.reply_video(video=open(video_file, 'rb'))
-            os.remove(video_file)  # Remover o vídeo após enviar
-        else:
-            await update.message.reply_text(
-                'Erro ao baixar o vídeo. Certifique-se de que o link é público e tente novamente.'
-            )
+        try:
+            video_file = download_video(url, download_path)
+            if video_file:
+                await update.message.reply_video(video=open(video_file, 'rb'))
+                os.remove(video_file)
+            else:
+                await update.message.reply_text('Erro ao baixar o vídeo. Certifique-se de que o link é público e tente novamente.')
+        finally:
+            # Limpa o diretório temporário
+            shutil.rmtree(download_path)
     else:
         await update.message.reply_text('Por favor, envie um link válido do YouTube.')
 
-# Função principal para iniciar o bot
 def main():
     application = Application.builder().token(TOKEN).build()
-
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_video))
 
